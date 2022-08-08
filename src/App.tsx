d14151191a978b9e4bf127a5ada0b41feda8eebf
theVoogie <vugar.abdulzada@webstep.no>
2022-08-08 15:31:48 +0200

    Initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,76 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const constainerRef = useRef<HTMLDivElement>(null);
+  const boxRef = useRef<HTMLDivElement>(null);
+
+  const isClicked = useRef<boolean>(false);
+
+  const coords = useRef<{
+    startX: number,
+    startY: number,
+    lastX: number,
+    lastY: number
+  }>({
+    startX: 0,
+    startY: 0,
+    lastX: 0,
+    lastY: 0
+  })
+
+  useEffect(() => {
+    if (!boxRef.current || !constainerRef.current) return;
+
+    const box = boxRef.current;
+    const container = constainerRef.current;
+
+
+    const onMouseDown = (e: MouseEvent) => {
+      isClicked.current = true;
+      coords.current.startX = e.clientX;
+      coords.current.startY = e.clientY;
+    }
+
+    const onMouseUp = (e: MouseEvent) => {
+      isClicked.current = false;
+      coords.current.lastX = box.offsetLeft;
+      coords.current.lastY = box.offsetTop;
+    }
+
+    const onMouseMove = (e: MouseEvent) => {
+      if (!isClicked.current) return;
+
+      const nextX = e.clientX - coords.current.startX + coords.current.lastX;
+      const nextY = e.clientY - coords.current.startY + coords.current.lastY;
+
+      box.style.top = `${nextY}px`;
+      box.style.left = `${nextX}px`;
+    }
+
+    box.addEventListener('mousedown', onMouseDown);
+    box.addEventListener('mouseup', onMouseUp);
+    container.addEventListener('mousemove', onMouseMove);
+    container.addEventListener('mouseleave', onMouseUp);
+
+    const cleanup = () => {
+      box.removeEventListener('mousedown', onMouseDown);
+      box.removeEventListener('mouseup', onMouseUp);
+      container.removeEventListener('mousemove', onMouseMove);
+      container.removeEventListener('mouseleave', onMouseUp);
+    }
+
+    return cleanup;
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div ref={constainerRef} className="container">
+        <div ref={boxRef} className="box"></div>
+      </div>
+    </main>
   );
 }
 
